e7f8b03a12956ac8be06359c47c617f996473afe
PetkoKuzmanov <57803685+PetkoKuzmanov@users.noreply.github.com>
2021-08-31 18:42:06 +0300

    Now gets the hourly forecast

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,9 @@ class _WeatherForecastState extends State<WeatherForecast> {
   List hourlyWeather = [];
   List dailyWeather = [];
 
+  var currentWeatherContainer = Container();
+  var hourlyWeatherContainer = Container();
+
   @override
   void initState() {
     super.initState();
@@ -52,43 +55,8 @@ class _WeatherForecastState extends State<WeatherForecast> {
             ],
           ),
         ),
-        getCurrentWeather(),
-        Container(
-          margin: const EdgeInsets.symmetric(vertical: 25.0, horizontal: 25.0),
-          child: Column(
-            children: [
-              Padding(
-                child: Text(
-                  "Hourly Forecast",
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                padding: EdgeInsets.all(10.0),
-              ),
-              Container(
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: [
-                        HourlyForecastWidget(),
-                        HourlyForecastWidget(),
-                        HourlyForecastWidget(),
-                        HourlyForecastWidget(),
-                        HourlyForecastWidget(),
-                        HourlyForecastWidget(),
-                        HourlyForecastWidget(),
-                        HourlyForecastWidget(),
-                      ],
-                    ),
-                  ))
-            ],
-          ),
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.blueAccent),
-            borderRadius: BorderRadius.all(Radius.circular(10.0)),
-          ),
-        ),
+        currentWeatherContainer,
+        hourlyWeatherContainer,
         Container(
           margin: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 25.0),
           child: Column(
@@ -127,9 +95,10 @@ class _WeatherForecastState extends State<WeatherForecast> {
     );
   }
 
-  void loadData() async {
+  Future<void> loadData() async {
     var url = Uri.parse(Strings.plovdivCall);
     var response = await http.get(url);
+
     setState(() {
       if (response.statusCode == 200) {
         var responseBody =
@@ -138,7 +107,9 @@ class _WeatherForecastState extends State<WeatherForecast> {
         currentWeather = responseBody["current"];
         hourlyWeather = responseBody["hourly"];
         dailyWeather = responseBody["daily"];
-        print(currentWeather);
+
+        currentWeatherContainer = getCurrentWeather();
+        hourlyWeatherContainer = getHourlyForecast();
 
         print('Request completed');
       } else {
@@ -147,7 +118,7 @@ class _WeatherForecastState extends State<WeatherForecast> {
     });
   }
 
-  Widget getCurrentWeather() {
+  Container getCurrentWeather() {
     return Container(
       child: Column(
         children: [
@@ -163,26 +134,85 @@ class _WeatherForecastState extends State<WeatherForecast> {
       ),
     );
   }
+
+  Container getHourlyForecast() {
+    List<Widget> hourWidgetList = [];
+    for (int i = 0; i < 24; i++) {
+      hourWidgetList.add(HourlyForecastWidget(hourlyWeather[i], i));
+    }
+
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 25.0, horizontal: 25.0),
+      child: Column(
+        children: [
+          Padding(
+            child: Text(
+              "Hourly Forecast",
+              style: TextStyle(fontSize: 20.0),
+            ),
+            padding: EdgeInsets.all(15.0),
+          ),
+          Container(
+              margin: const EdgeInsets.fromLTRB(0.0, 0.0, 10.0, 0.0),
+              child: SingleChildScrollView(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: hourWidgetList,
+                ),
+              ))
+        ],
+      ),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.blueAccent),
+        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+      ),
+    );
+  }
 }
 
-class HourlyForecastWidget extends StatelessWidget {
-  const HourlyForecastWidget({Key? key}) : super(key: key);
+class HourlyForecastWidget extends StatefulWidget {
+  const HourlyForecastWidget(this.hourlyForecast, this.position, {Key? key})
+      : super(key: key);
+
+  final Map<String, dynamic> hourlyForecast;
+  final position;
 
   @override
+  _HourlyForecastWidgetState createState() => _HourlyForecastWidgetState();
+}
+
+class _HourlyForecastWidgetState extends State<HourlyForecastWidget> {
+  @override
   Widget build(BuildContext context) {
+    var time = "Now";
+    String image;
+
+    if (widget.position != 0) {
+      var dateTime = DateTime.fromMillisecondsSinceEpoch(
+          widget.hourlyForecast["dt"] * 1000);
+      time = dateTime.hour.toString() + ":00";
+    }
+
+    image = widget.hourlyForecast["weather"][0]["icon"].toString();
+
     return Padding(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
-            "Now",
+            time,
             style: TextStyle(fontSize: 15.0),
           ),
           SizedBox(height: 10),
-          Icon(Icons.wb_sunny_outlined),
+          Image.asset(
+            "assets/images/$image.png",
+            width: 50,
+            height: 50,
+          ),
           SizedBox(height: 10),
           Text(
-            "30°",
+            widget.hourlyForecast["temp"].toString().substring(0, 2) + "°",
             style: TextStyle(fontSize: 15.0),
           ),
           SizedBox(height: 10),
